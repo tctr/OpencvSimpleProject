0758319faf8e169fd4e03495b4c73d6296bf88f8
tctr <thierry.coutier@gmail.com>
2023-07-12 11:30:55 +0200

    after going to build and launching the command  cmake ..

---
+++ build/CMakeFiles/simple-demo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple-demo.dir/main.cpp.o"
+  "CMakeFiles/simple-demo.dir/main.cpp.o.d"
+  "simple-demo"
+  "simple-demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple-demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
